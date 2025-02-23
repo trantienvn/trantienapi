96d09bf463714493dbd72f3a07686768e797317e
trantienvn <tientn2005@gmail.com>
2025-02-23 15:46:42 +0700

    Revert "fix date"
    This reverts commit 9af7e723a7ce881c7e500f2f26644c6072942272.

--- app/api/filelms/route.ts
+++ app/api/filelms/route.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 // Hàm định dạng thời gian (thay thế $o().format)
 const formatDate = (date: Date): string => {
-  date.setHours(date.getHours() + 7);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
